8706c7aafc491e98eac9bd52ca0c7bdb443136c6
Felix <youremail@gmail.com>
2026-07-01 21:26:39 -0400

    Load dist handler directly in cron entrypoints

--- api/cron/evening-reminder.ts
+++ api/cron/evening-reminder.ts
@@ -1,3 +1,4 @@
-import handler from "../[...slug]";
+// @ts-ignore Generated at build time; no static .d.ts emitted for this JS bundle.
+import handler from "../../dist/vercel-handler.js";
 
 export default handler;
